3255846cb924ea1c6b156aae0cdaf671bd7b4a56
Andres Arias <andres.arias12@gmail.com>
2023-11-12 17:58:27 -0600

    Added lualine and tmux integration for nvim

--- nvim/lua/ariasva/remaps.lua
+++ nvim/lua/ariasva/remaps.lua
@@ -2,6 +2,8 @@ vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+
 -- Quickly insert an empty new line without entering insert mode
 vim.keymap.set("n", "<Leader>o", "o<Esc>")
 -- Adds a semicolon to the end of the line.
